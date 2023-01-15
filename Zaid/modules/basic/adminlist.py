2996ec82ede53e3171dc3c207b9e9a0e4c62b82e
xlucifer711 <106505627+xlucifer711@users.noreply.github.com>
2023-01-15 21:25:57 +0200

    Add files via upload

---
+++ Zaid/modules/basic/adminlist.py
@@ -0,0 +1,194 @@
+import html
+
+from pyrogram import Client, enums, filters
+from pyrogram.types import Message
+
+from Zaid.helper.basic import edit_or_reply
+from Zaid.helper.parser import mention_html, mention_markdown
+from Zaid.modules.help import *
+
+
+@Client.on_message(filters.me & filters.command(["المشرفين", "الادمن"], "."))
+async def adminlist(client: Client, message: Message):
+    replyid = None
+    toolong = False
+    if len(message.text.split()) >= 2:
+        chat = message.text.split(None, 1)[1]
+        grup = await client.get_chat(chat)
+    else:
+        chat = message.chat.id
+        grup = await client.get_chat(chat)
+    if message.reply_to_message:
+        replyid = message.reply_to_message.id
+    creator = []
+    admin = []
+    badmin = []
+    async for a in client.get_chat_members(
+        message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS
+    ):
+        try:
+            nama = a.user.first_name + " " + a.user.last_name
+        except:
+            nama = a.user.first_name
+        if nama is None:
+            nama = "☠️ حساب محذوف"
+        if a.status == enums.ChatMemberStatus.ADMINISTRATOR:
+            if a.user.is_bot:
+                badmin.append(mention_markdown(a.user.id, nama))
+            else:
+                admin.append(mention_markdown(a.user.id, nama))
+        elif a.status == enums.ChatMemberStatus.OWNER:
+            creator.append(mention_markdown(a.user.id, nama))
+    admin.sort()
+    badmin.sort()
+    totaladmins = len(creator) + len(admin) + len(badmin)
+    teks = "**Admins in {}**\n".format(grup.title)
+    teks += "╒═══「 الــمــالـڪ 」\n"
+    for x in creator:
+        teks += "│ • {}\n".format(x)
+        if len(teks) >= 4096:
+            await message.reply(message.chat.id, teks, reply_to_message_id=replyid)
+            teks = ""
+            toolong = True
+    teks += "╞══「 {} الـمــشــࢪفــيــنـ 」\n".format(len(admin))
+    for x in admin:
+        teks += "│ • {}\n".format(x)
+        if len(teks) >= 4096:
+            await message.reply(message.chat.id, teks, reply_to_message_id=replyid)
+            teks = ""
+            toolong = True
+    teks += "╞══「 {} البــوتــاتــ الـمــشــࢪفــيــنـ 」\n".format(len(badmin))
+    for x in badmin:
+        teks += "│ • {}\n".format(x)
+        if len(teks) >= 4096:
+            await message.reply(message.chat.id, teks, reply_to_message_id=replyid)
+            teks = ""
+            toolong = True
+    teks += "╘══「 عــدد {} الـمــشــࢪفــيــنـ 」".format(totaladmins)
+    if toolong:
+        await message.reply(message.chat.id, teks, reply_to_message_id=replyid)
+    else:
+        await message.edit(teks)
+
+
+@Client.on_message(
+    filters.me & filters.command(["ابلاغ", "بلغ الادمن", "بلاغ"], ".")
+)
+async def report_admin(client: Client, message: Message):
+    await message.delete()
+    if len(message.text.split()) >= 2:
+        text = message.text.split(None, 1)[1]
+    else:
+        text = None
+    grup = await client.get_chat(message.chat.id)
+    admin = []
+    async for a in client.get_chat_members(
+        message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS
+    ):
+        if (
+            a.status == enums.ChatMemberStatus.ADMINISTRATOR
+            or a.status == enums.ChatMemberStatus.OWNER
+        ):
+            if not a.user.is_bot:
+                admin.append(mention_html(a.user.id, "\u200b"))
+    if message.reply_to_message:
+        if text:
+            teks = "{}".format(text)
+        else:
+            teks = "{} reported to admins.".format(
+                mention_html(
+                    message.reply_to_message.from_user.id,
+                    message.reply_to_message.from_user.first_name,
+                )
+            )
+    else:
+        if text:
+            teks = "{}".format(html.escape(text))
+        else:
+            teks = "Calling admins in {}.".format(grup.title)
+    teks += "".join(admin)
+    if message.reply_to_message:
+        await client.send_message(
+            message.chat.id,
+            teks,
+            reply_to_message_id=message.reply_to_message.id,
+            parse_mode=enums.ParseMode.HTML,
+        )
+    else:
+        await client.send_message(
+            message.chat.id, teks, parse_mode=enums.ParseMode.HTML
+        )
+
+
+@Client.on_message(filters.me & filters.command(["تاج للكل", "mentionall"], "."))
+async def tag_all_users(client: Client, message: Message):
+    await message.delete()
+    if len(message.text.split()) >= 2:
+        text = message.text.split(None, 1)[1]
+    else:
+        text = "هلوو  🙃"
+    kek = client.get_chat_members(message.chat.id)
+    async for a in kek:
+        if not a.user.is_bot:
+            text += mention_html(a.user.id, "\u200b")
+    if message.reply_to_message:
+        await client.send_message(
+            message.chat.id,
+            text,
+            reply_to_message_id=message.reply_to_message.id,
+            parse_mode=enums.ParseMode.HTML,
+        )
+    else:
+        await client.send_message(
+            message.chat.id, text, parse_mode=enums.ParseMode.HTML
+        )
+
+
+@Client.on_message(filters.me & filters.command(["قايمه البوتات", "البوتات"], "."))
+async def get_list_bots(client: Client, message: Message):
+    replyid = None
+    if len(message.text.split()) >= 2:
+        chat = message.text.split(None, 1)[1]
+        grup = await client.get_chat(chat)
+    else:
+        chat = message.chat.id
+        grup = await client.get_chat(chat)
+    if message.reply_to_message:
+        replyid = message.reply_to_message.id
+    getbots = client.get_chat_members(chat)
+    bots = []
+    async for a in getbots:
+        try:
+            nama = a.user.first_name + " " + a.user.last_name
+        except:
+            nama = a.user.first_name
+        if nama is None:
+            nama = "☠️ Deleted account"
+        if a.user.is_bot:
+            bots.append(mention_markdown(a.user.id, nama))
+    teks = "**All bots in group {}**\n".format(grup.title)
+    teks += "╒═══「 البــوتــاتــ الـمــشــࢪفــيــنـ 」\n"
+    for x in bots:
+        teks += "│ • {}\n".format(x)
+    teks += "╘══「 عــدد {} البــوتــاتــ 」".format(len(bots))
+    if replyid:
+        await client.send_message(message.chat.id, teks, reply_to_message_id=replyid)
+    else:
+        await message.edit(teks)
+
+
+add_command_help(
+    "tag",
+    [
+        [f".المشرفين", "الادمن ال في البار."],
+        [f".صفي الحذفين or .الحذفين", "طرد ال حسبتهم محذوفه."],
+        [
+            f".تاج للكل `or` .mentionall",
+            "تعمل تاج للناس ",
+        ],
+        [
+            f".قايمه البوتات",
+            "البوتات ال في البار",
+        ],
+    ],
+)
---
+++ Zaid/modules/basic/afk.py
@@ -0,0 +1,143 @@
+import asyncio
+from datetime import datetime
+
+import humanize
+from pyrogram import filters, Client
+from pyrogram.types import Message
+
+from Zaid.helper.PyroHelpers import GetChatID, ReplyCheck
+from Zaid.modules.help import add_command_help
+
+AFK = False
+AFK_REASON = ""
+AFK_TIME = ""
+USERS = {}
+GROUPS = {}
+
+
+def subtract_time(start, end):
+    subtracted = humanize.naturaltime(start - end)
+    return str(subtracted)
+
+
+@Client.on_message(
+    ((filters.group & filters.mentioned) | filters.private) & ~filters.me & ~filters.service, group=3
+)
+async def collect_afk_messages(bot: Client, message: Message):
+    if AFK:
+        last_seen = subtract_time(datetime.now(), AFK_TIME)
+        is_group = True if message.chat.type in ["supergroup", "group"] else False
+        CHAT_TYPE = GROUPS if is_group else USERS
+
+        if GetChatID(message) not in CHAT_TYPE:
+            text = (
+                f"`لقد اتيت 😇♩ لا مزيد من الانشغال .\nلقد كنت مشغولا لمدة .\n"
+                f"فنا مش قافل دلوقتي.\n"
+                f"اخر ظهور: {last_seen}\n"
+                f"السبب: ```{AFK_REASON.upper()}```\n"
+                f"هرتاح شويه وافتح تني.`"
+            )
+            await bot.send_message(
+                chat_id=GetChatID(message),
+                text=text,
+                reply_to_message_id=ReplyCheck(message),
+            )
+            CHAT_TYPE[GetChatID(message)] = 1
+            return
+        elif GetChatID(message) in CHAT_TYPE:
+            if CHAT_TYPE[GetChatID(message)] == 50:
+                text = (
+                    f"`This is an automated message\n"
+                    f"Last seen: {last_seen}\n"
+                    f"This is the 10th time I've told you I'm AFK right now..\n"
+                    f"I'll get to you when I get to you.\n"
+                    f"No more auto messages for you`"
+                )
+                await bot.send_message(
+                    chat_id=GetChatID(message),
+                    text=text,
+                    reply_to_message_id=ReplyCheck(message),
+                )
+            elif CHAT_TYPE[GetChatID(message)] > 50:
+                return
+            elif CHAT_TYPE[GetChatID(message)] % 5 == 0:
+                text = (
+                    f"`Hey I'm still not back yet.\n"
+                    f"Last seen: {last_seen}\n"
+                    f"Still busy: ```{AFK_REASON.upper()}```\n"
+                    f"Try pinging a bit later.`"
+                )
+                await bot.send_message(
+                    chat_id=GetChatID(message),
+                    text=text,
+                    reply_to_message_id=ReplyCheck(message),
+                )
+
+        CHAT_TYPE[GetChatID(message)] += 1
+
+
+@Client.on_message(filters.command("afk", ".") & filters.me, group=3)
+async def afk_set(bot: Client, message: Message):
+    global AFK_REASON, AFK, AFK_TIME
+
+    cmd = message.command
+    afk_text = ""
+
+    if len(cmd) > 1:
+        afk_text = " ".join(cmd[1:])
+
+    if isinstance(afk_text, str):
+        AFK_REASON = afk_text
+
+    AFK = True
+    AFK_TIME = datetime.now()
+
+    await message.delete()
+
+
+@Client.on_message(filters.command("afk", "!") & filters.me, group=3)
+async def afk_unset(bot: Client, message: Message):
+    global AFK, AFK_TIME, AFK_REASON, USERS, GROUPS
+
+    if AFK:
+        last_seen = subtract_time(datetime.now(), AFK_TIME).replace("ago", "").strip()
+        await message.edit(
+            f"`While you were away (for {last_seen}), you received {sum(USERS.values()) + sum(GROUPS.values())} "
+            f"messages from {len(USERS) + len(GROUPS)} chats`"
+        )
+        AFK = False
+        AFK_TIME = ""
+        AFK_REASON = ""
+        USERS = {}
+        GROUPS = {}
+        await asyncio.sleep(5)
+
+    await message.delete()
+
+if AFK:
+   @Client.on_message(filters.me, group=3)
+   async def auto_afk_unset(bot: Client, message: Message):
+       global AFK, AFK_TIME, AFK_REASON, USERS, GROUPS
+
+       if AFK:
+           last_seen = subtract_time(datetime.now(), AFK_TIME).replace("ago", "").strip()
+           reply = await message.reply(
+               f"`While you were away (for {last_seen}), you received {sum(USERS.values()) + sum(GROUPS.values())} "
+               f"messages from {len(USERS) + len(GROUPS)} chats`"
+           )
+           AFK = False
+           AFK_TIME = ""
+           AFK_REASON = ""
+           USERS = {}
+           GROUPS = {}
+           await asyncio.sleep(5)
+           await reply.delete()
+
+
+add_command_help(
+    "afk",
+    [
+        [".نايم", "لو هتنام اعمله هيعرف الناس انك نايم ```.نايم <السبب>```"],
+        ["!صحيت", "لتعطيله."],
+    ],
+)
